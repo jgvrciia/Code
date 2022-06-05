7de5141e5eda8c5fd4b78d836b46b5711d2c3563
Juan <jgarcia.rwc@gmail.com>
2022-06-05 12:12:40 -0700

    Added logic

--- Coffee.py
+++ Coffee.py
@@ -1,15 +1,27 @@
-# Let's build a robot barrista
+# Let's build a robot barista
 import time
 
 print("Hello, welcome to Baytech Coffee!!!\n\n\n")
 time.sleep(3)
 name = input("What is your name?\n> ")
 
+# We don't want Diego's in our shop
+if name == "Diego":
+    evil = input("Are you evil? (yes/no)\n> ")
+    if evil == "Yes".casefold():
+        print("GET OUT EVIL DIEGO!! YOU'RE BANNED FROM THIS COFFEE SHOP!!")
+        exit()
+
 print("Hello, " + name + " thank you so much for coming in today\n")
 time.sleep(1)
-menu = "Coffee, Espresso, Frappuccino, Latte, Tea, Cappucino"
+menu = "Coffee: $3 \nEspresso: $5 \nFrappuccino: $7 \nLatte: $6 \nTea: $2 \nCappuccino: $4\n"
 
-order = input(("What would you like to drink? Today's Menu is\n" + menu + "\n> "))
+order = input(("What would you like to drink? Today's Menu is\n" + menu + "\n> ")).casefold()
+
+while order != "Coffee".casefold() and order != "Espresso".casefold() and order != "Frappuccino".casefold() and order != "Latte".casefold() and order != "Tea".casefold() and order != "Cappuccino".casefold():
+    print("Please Choose a menu option")
+    order = input(("What would you like to drink? Today's Menu is\n" + menu + "\n> ")).casefold()
+
 
 print("Sounds good " + name)
 
@@ -19,26 +31,26 @@ Espresso_price = 5
 Frappuccino_price = 7
 Latte_price = 6
 Tea_price = 2
-Cappucino_price = 4
+Cappuccino_price = 4
 
-ammount = input("How many " + order + "'s would you like?\n> ")
+amount = input("How many " + order + "'s would you like?\n> ")
 
 
-if order == "Coffee":
-    total = Coffee_price * int(ammount)
-if order == "Espresso":
-    total = Espresso_price * int(ammount)
-if order == "Frappuccino":
-    total = Frappuccino_price * int(ammount)
-if order == "Latte":
-    total = Latte_price * int(ammount)
-if order == "Tea":
-    total = Tea_price * int(ammount)
-if order == "Cappucino":
-    total = Cappucino_price * int(ammount)
+if order == "Coffee".casefold():
+    total = Coffee_price * int(amount)
+if order == "Espresso".casefold():
+    total = Espresso_price * int(amount)
+if order == "Frappuccino".casefold():
+    total = Frappuccino_price * int(amount)
+if order == "Latte".casefold():
+    total = Latte_price * int(amount)
+if order == "Tea".casefold():
+    total = Tea_price * int(amount)
+if order == "Cappuccino".casefold():
+    total = Cappuccino_price * int(amount)
 
 print("Thank you. Your total is: $" + str(total) + ".00\n")
 
 time.sleep(3)
 
-print("Thank you " + name + ", We'll have your " + ammount + " " + order + "'s ready in a few minutes")
+print("Thank you " + name + ", We'll have your " + amount + " " + order + "'s ready in a few minutes")
