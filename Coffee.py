91385b4fc9d13fcd406d583c3ea1b1d3722a2d47
Juan <jgarcia.rwc@gmail.com>
2022-06-05 09:48:46 -0700

    Coffee shhop

--- Coffee.py
+++ Coffee.py
@@ -2,3 +2,15 @@
 
 print("Hello, welcome to Baytech Coffee!!!")
 
+name = input("What is your name?\n")
+
+print("Hello, " + name + " thank you so much for coming in today\n")
+
+menu = "Black Coffee, Decaf, Caramel Frappuccino, Chai Latte, Iced Tea"
+
+order = input(("What would you like to drink? Today's Menu is\n" + menu + "\n>"))
+
+print("Sounds good " + name + ", we'll have your " + order + " ready in a moment")
+
+
+
