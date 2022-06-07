de7e633f6ec5036f2d928bedde7ce48fc8f4f504
jgvrciia <jgvrciia@gmail.com>
2022-06-07 10:06:47 -0700

    Added logic

--- Coffee.py
+++ Coffee.py
@@ -1,31 +1,33 @@
 # Let's build a robot barista
 import time
 
-print("Hello, welcome to Baytech Coffee!!!\n\n\n")
+print("Hello, Welcome to Baytech Coffee!!!\n\n\n")
 time.sleep(3)
 name = input("What is your name?\n> ")
 
-# We don't want Diego's in our shop
-if name == "Diego":
+# We don't want Evil Diego's or Eddie's in our shop
+if name.capitalize() == "Diego" or name.capitalize() == "Eddie":
     evil = input("Are you evil? (yes/no)\n> ")
     if evil == "Yes".casefold():
-        print("GET OUT EVIL DIEGO!! YOU'RE BANNED FROM THIS COFFEE SHOP!!")
+        print("GET OUT EVIL " + name + "!! YOU'RE BANNED FROM THIS COFFEE SHOP!!")
         exit()
+    else:
+        print("Sorry for the confusion we have an evil Diego roaming around")
 
-print("Hello, " + name + " thank you so much for coming in today\n")
+print("Hello, " + name.capitalize() + " thank you so much for coming in today\n")
 time.sleep(1)
 menu = "Coffee: $3 \nEspresso: $5 \nFrappuccino: $7 \nLatte: $6 \nTea: $2 \nCappuccino: $4\n"
 
 order = input(
-    ("What would you like to drink? Today's Menu is\n" + menu + "\n> ")).casefold()
+    ("What would you like to drink? Today's Menu is:\n" + menu + "\n> ")).casefold()
 
 while order != "Coffee".casefold() and order != "Espresso".casefold() and order != "Frappuccino".casefold() and order != "Latte".casefold() and order != "Tea".casefold() and order != "Cappuccino".casefold():
-    print("Please Choose a menu option")
+    print("Sorry we don't make that here. Please choose a menu option")
     order = input(
         ("What would you like to drink? Today's Menu is\n" + menu + "\n> ")).casefold()
 
 
-print("Sounds good " + name)
+print("Sounds good " + name.capitalize())
 
 
 Coffee_price = 3
@@ -56,4 +58,5 @@ print("Thank you. Your total is: $" + str(total) + ".00\n")
 time.sleep(3)
 
 print("Thank you " + name + ", We'll have your " +
-      amount + " " + order + "'s ready in a few minutes")
+      amount + " " + order + "'s ready soon")
+
