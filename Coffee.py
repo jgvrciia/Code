d97faf20e7714b313af93f8279df4efad623b6bf
jgvrciia <jgvrciia@gmail.com>
2022-06-07 22:28:07 -0700

    added logic at end

--- Coffee.py
+++ Coffee.py
@@ -6,25 +6,25 @@ time.sleep(3)
 name = input("What is your name?\n> ")
 
 # We don't want Evil Diego's or Eddie's in our shop
-if name.capitalize() == "Diego" or name.capitalize() == "Eddie":
+if name.capitalize() == "Diego" or name.capitalize() == "Duc":
     evil = input("Are you evil? (yes/no)\n> ")
-    if evil == "Yes".casefold():
-        print("GET OUT EVIL " + name + "!! YOU'RE BANNED FROM THIS COFFEE SHOP!!")
+    if evil.casefold() == "yes":
+        print("GET OUT EVIL " + name.upper() + "!! YOU'RE BANNED FROM THIS COFFEE SHOP!!")
         exit()
     else:
-        print("Sorry for the confusion we have an evil Diego roaming around")
+        print("Sorry for the confusion we have an evil " + name.capitalize() + " roaming around")
 
 print("Hello, " + name.capitalize() + " thank you so much for coming in today\n")
 time.sleep(1)
 menu = "Coffee: $3 \nEspresso: $5 \nFrappuccino: $7 \nLatte: $6 \nTea: $2 \nCappuccino: $4\n"
 
 order = input(
-    ("What would you like to drink? Today's Menu is:\n" + menu + "\n> ")).casefold()
+    ("What would you like to drink? Today's menu is:\n" + menu + "\n> ")).casefold()
 
 while order != "Coffee".casefold() and order != "Espresso".casefold() and order != "Frappuccino".casefold() and order != "Latte".casefold() and order != "Tea".casefold() and order != "Cappuccino".casefold():
     print("Sorry we don't make that here. Please choose a menu option")
     order = input(
-        ("What would you like to drink? Today's Menu is\n" + menu + "\n> ")).casefold()
+        ("What would you like to drink? Today's menu is\n" + menu + "\n> ")).casefold()
 
 
 print("Sounds good " + name.capitalize())
@@ -37,7 +37,7 @@ Latte_price = 6
 Tea_price = 2
 Cappuccino_price = 4
 
-amount = input("How many " + order + "'s would you like?\n> ")
+amount = input("How many " + order.capitalize() + "'s would you like?\n> ")
 
 
 if order == "Coffee".casefold():
@@ -57,6 +57,9 @@ print("Thank you. Your total is: $" + str(total) + ".00\n")
 
 time.sleep(3)
 
-print("Thank you " + name + ", We'll have your " +
-      amount + " " + order + "'s ready soon")
+if amount == "1":
+    print("Thank you " + name + ", We'll have your " + order + " ready soon")
+else:
+    print("Thank you " + name + ", We'll have your " +
+          amount + " " + order + "'s ready soon")
 
