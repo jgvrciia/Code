34a7dd9a9f3767641b98ccdd46c951e3e6847e37
Juan <jgarcia.rwc@gmail.com>
2022-06-08 19:58:03 -0700

    Update Camping.py

---
+++ Camping.py
@@ -0,0 +1,9 @@
+# Let's Work With Python Lists
+
+SUPPLIES = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "toiletries", "food"]
+
+CAMP_SITE = ["MT. Shasta", 404, 95.5, 10, False]
+
+BERNARD = SUPPLIES[0]
+
+print(BERNARD)
